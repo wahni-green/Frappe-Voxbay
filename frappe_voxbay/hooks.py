9beeb5fc2a9a61c3f95ff7837566363947077f02
niyazrazak <niyasibnurazak@gmail.com>
2022-12-07 10:36:59 +0000

    feat: call from lead

--- frappe_voxbay/hooks.py
+++ frappe_voxbay/hooks.py
@@ -29,7 +29,7 @@ app_include_js = "/assets/frappe_voxbay/js/utils.js"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {"Lead" : "public/js/lead.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
